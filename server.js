dbd6269026cc22e5d81f42557727fed872c4ec72
GuillianV <guillianvibert90@gmail.com>
2021-05-15 14:45:27 +0200

    Add gitignoreJs (for personal datas) & add modules folder

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ let app = express()
 let http = require("http");
 let server = http.Server(app)
 
+let gitignore = require("./modules/gitignore.js")
+
+// let { Pool, Client } = require('pg');
+// let myDatabase = require("./modules/databaseConnect.js")
 
 app.use(express.static(__dirname + "/public-parcel/dist"))
 app.use(express.static(__dirname + "/public-parcel"))
@@ -19,4 +23,6 @@ app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/public-parcel/html/about.html")
 })
 // Listen on port 8080
-app.listen(8888);
+app.listen(gitignore.listenPort, function(){
+    console.log("Listening at port : "+gitignore.listenPort)
+});
