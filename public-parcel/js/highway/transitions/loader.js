9c0a22d04b9a84f35a608f681c019b9c07cf44a6
GuillianV <guillianvibert90@gmail.com>
2021-05-17 16:16:52 +0200

    end loader highway and start responsive for header

--- public-parcel/js/highway/transitions/loader.js
+++ public-parcel/js/highway/transitions/loader.js
@@ -2,6 +2,11 @@ import Highway from '@dogstudio/highway'
 import  gsap from 'gsap';
 
 
+// let screenWidth = window.screen.height;
+// let screenHeight = window.screen.width;
+// let screenHypotenuse = Math.sqrt(Math.pow(screenWidth,2) + Math.pow(screenHeight,2))
+
+
 class Loader extends Highway.Transition {
 
     in({ from, to, done }) {
@@ -30,38 +35,34 @@ function LoaderIn(done,loader){
     let loaderIn = gsap.timeline();
 
     loaderIn.set(loader, {
-        xPercent: -35,
-        yPercent: 90,
-        rotate:45,
+        xPercent: -100,
 
     });
 
     loaderIn.to(loader, {
         xPercent: 0,
-        yPercent: 0,
-        rotate:45,
         duration: 1.5,
         ease: "power2.out",
-        onComplete: done,
+        onComplete:done
     });
+
+
 }
 
 export { LoaderIn };
 
 function LoaderOut(loader){
+
+
     let loaderOut = gsap.timeline();
 
     loaderOut.to(loader, {
-        xPercent: 35,
-        yPercent: -90,
-        rotate:45,
+        xPercent: 100,
         duration: 3, 
         ease: "power4.out",
     });
     loaderOut.set(loader, {
-        xPercent: -35,
-        yPercent: 90,
-        rotate:45,
+        xPercent: -100,
         delay:2,
     });
 }
