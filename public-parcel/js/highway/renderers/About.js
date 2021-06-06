0f3afb1eb457341a4f026672d921ff3952bb71d7
GuillianV <guillianvibert90@gmail.com>
2021-06-06 12:17:55 +0200

    add svg affects and images & try responsive

--- public-parcel/js/highway/renderers/About.js
+++ public-parcel/js/highway/renderers/About.js
@@ -1,10 +1,10 @@
 import Highway from '@dogstudio/highway'
-import  gsap, { Expo } from 'gsap';
+import gsap, { Expo } from 'gsap';
 import { MotionPathPlugin } from 'gsap/MotionPathPlugin'
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 // import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-gsap.registerPlugin(MotionPathPlugin, ScrollToPlugin, ScrollTrigger )
+gsap.registerPlugin(MotionPathPlugin, ScrollToPlugin, ScrollTrigger)
 //Load user controls
 import LoadHeader from '../userControls/header'
 import LoadFooter from '../userControls/footer'
@@ -27,43 +27,48 @@ class About extends Highway.Renderer {
         LoadFooter()
 
         const tl = gsap.timeline();
-        tl.to(".layer1",5,{y:400})
-        .to(".layer2",5,{y:-500},'-=5')
-        .to(".layer3",5,{y:-200},'-=5')
-        .to(".layer4",5,{y:200},'-=5')
-        .to(".layer5",5,{y:500},'-=5')
-        .to(".layer6",5,{y:-500},'-=5')
-        .to(".parallax-bottom",5,{y:-500},'-=5')
+        tl.to(".layer1", 5, { y: -500 })
+            .to(".layer2", 5, { y: -500 }, '-=5')
+            .to(".layer3", 5, { y: -200 }, '-=5')
+            .to(".layer4", 5, { y: 200 }, '-=5')
+            .to(".layer5", 5, { y: 500 }, '-=5')
+            .to(".layer6", 5, { y: -500 }, '-=5')
+            .to(".parallax-bottom", 5, { y: -500 }, '-=5')
 
         ScrollTrigger.create({
-            animation:tl,
-            trigger:".parallax-section",
-            markers:false,
-            start:"top 50%",
-            end:"+=2000",
-            scrub:true
+            animation: tl,
+            trigger: ".parallax-section",
+            markers: false,
+            start: "top 50%",
+            end: "+=2000",
+            scrub: true
         })
 
 
 
 
-        
-gsap.set(".pathBall", {xPercent: -50, yPercent: -50, x: 0});
 
-var action = gsap.timeline({defaults: {duration: 1},
-  scrollTrigger: {
-    trigger: ".content-block",
-    endTrigger:"footer",
-    markers:true,
-    scrub: true,
-    start: "top center",
-    end: "bottom center"
-  }})
-  .fromTo(".pathLine", {drawSVG: "100% 100%"}, {drawSVG: "0% 100%"}, 0)
-.from(".pathBall", {motionPath: {path: ".pathLine",align:".pathLine", autoRotate:225, alignOrigin: [0.5, 0.5]}}, 0);
+        gsap.set(".pathBall", { xPercent: -50, yPercent: -50, x: 0 });
+
+        var action = gsap.timeline({
+            defaults: { duration: 1 },
+            scrollTrigger: {
+                trigger: ".content-block",
+                endTrigger: "footer",
+                markers: true,
+                scrub: true,
+                start: "top center",
+                end: "bottom center",
+        
+            }
+        })
+            .fromTo(".pathLine", { drawSVG: "100% 100%" }, { drawSVG: "0% 100%" }, 0)
+            .from(".pathBall", { motionPath: { path: ".pathLine", opacity:1, align: ".pathLine", autoRotate: 20, alignOrigin: [0.5, 0.5] } }, 0)
+           
+       
     }
 
-    onLeave() {
+    onLeave() { 
         //Javascript a lancer lorsque la page part/unload
     }
 
