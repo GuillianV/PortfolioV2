fd08958a309ce87068d99bd4a35814255987d287
GuillianV <guillianvibert90@gmail.com>
2021-06-15 15:59:58 +0200

    add popup experiences

--- public-parcel/js/highway/renderers/Projects.js
+++ public-parcel/js/highway/renderers/Projects.js
@@ -1,6 +1,9 @@
 import Highway from '@dogstudio/highway'
-import  gsap from 'gsap';
+import gsap from 'gsap';
 import * as Swiper from 'swiper'
+import { Navigation } from 'swiper';
+
+Swiper.Swiper.use([Navigation]);
 
 //Load user controls
 import LoadHeader from '../userControls/header'
@@ -30,12 +33,92 @@ class Projects extends Highway.Renderer {
             effect: 'fade',
             fadeEffect: { crossFade: true },
             spaceBetween: 30,
-            loop: true,
             grabCursor: true,
+            observer: true,
+            observeParents: true,
             speed: 600,
+            navigation: {
+                nextEl: '.swiper-projet-right',
+                prevEl: '.swiper-projet-left',
+            },
 
         })
-        swiperProjects.init()
+
+
+        let experiences = document.querySelectorAll('[data-experiences-popup]')
+        for (let experience of experiences) {
+            experience.addEventListener("click", function () {
+                
+                let fixed = document.querySelectorAll(".ct-fixed")
+                for (let fix of fixed) {
+                    fix.style.transform = "scale(1)"
+                }
+
+               
+                if (experience.dataset.experiencesPopup == "autoit") {
+
+                    let imgs = [
+                        require("../../../assets/img/header-bg.png"),
+                        require("../../../assets/img/guillian-back.png"),
+                        require("../../../assets/icons/fenec.png"),
+                    ]
+                    LoadCtPopup(imgs,experience.dataset.experiencesPopup)
+                }
+
+                
+
+               
+            });
+        }
+
+
+        function LoadCtPopup(images,dataset){
+            let wrapper = document.querySelector(".swiper-wrapper");
+            wrapper.innerHTML = "";
+
+
+
+            for (let img of images) {
+                var newDiv = document.createElement("div");
+                newDiv.classList.add("swiper-slide");
+                var newImg = document.createElement("img");
+                newImg.classList.add("image-cover")
+                newDiv.appendChild(newImg)
+                newImg.src = img
+                wrapper.appendChild(newDiv)
+            }
+
+            let eleme = document.querySelectorAll("[data-experiences-popup-"+dataset+"]")
+            let title = document.querySelector("#popup-title")
+            let desc = document.querySelector("#popup-description")
+            let tech = document.querySelector("#popup-technologie")
+            
+            for(let elem of eleme){
+               if(elem.dataset.popupTitle == "popup-title"){
+                title.innerHTML = elem.innerHTML
+               }
+               if(elem.dataset.popupTitle == "popup-description"){
+                desc.innerHTML = elem.innerHTML
+               }
+               if(elem.dataset.popupTitle == "popup-technologie"){
+                tech.innerHTML = elem.innerHTML
+               }
+            }
+            swiperProjects.init()
+        }
+
+
+
+
+        let ct_cross = document.querySelector('.ct-fixed-cross')
+
+        ct_cross.addEventListener("click", function () {
+            let fixed = document.querySelectorAll(".ct-fixed")
+            for (let fix of fixed) {
+                fix.style.transform = "scale(0)"
+            }
+        });
+
 
 
     }
